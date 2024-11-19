aec6d36542e3ba8cac0a599e04d9df1efcc28f40
Maxime Moreillon <m.moreillon@gmail.com>
2024-11-19 09:02:18 +0900

    fixed path update

--- src/configHandler.ts
+++ src/configHandler.ts
@@ -1,4 +1,3 @@
-import YAML from "yaml"
 import fs from "fs"
 import { app, safeStorage } from "electron"
 import path from "path"
@@ -9,8 +8,6 @@ const configPath = path.join(app.getPath("userData"), "config")
 
 export const loadConfig = () => {
   try {
-    // const file = fs.readFileSync(configPath, "utf8")
-    // const config = YAML.parse(file)
     const file = fs.readFileSync(configPath)
     const decryptedConfigString = safeStorage.decryptString(file)
     const config = JSON.parse(decryptedConfigString)
@@ -21,14 +18,12 @@ export const loadConfig = () => {
 }
 
 export const writeConfig = async (config: Settings) => {
-  // TODO: fix path update
   const { path: oldPath } = loadConfig()
   watcher.unwatch(oldPath)
-  // const yml = YAML.stringify(config)
-  // fs.writeFileSync(configPath, yml)
   const configString = JSON.stringify(config)
   const encryptedConfigString = safeStorage.encryptString(configString)
 
   fs.writeFileSync(configPath, encryptedConfigString)
-  const newConfig = loadConfig()
+  const { path: newPath } = loadConfig()
+  watcher.add(newPath)
 }
